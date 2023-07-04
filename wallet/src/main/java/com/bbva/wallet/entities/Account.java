8dfb0a70b273451c0fa8fced9b8661676c987ddc
robyspr <robysprenger@gmail.com>
2023-07-04 11:53:25 -0300

    BBVAFSW3T3-19 | Agregar entity Account

---
+++ wallet/src/main/java/com/bbva/wallet/entities/Account.java
@@ -0,0 +1,53 @@
+package com.bbva.wallet.entities;
+
+import com.bbva.wallet.enums.Currency;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "accounts")
+public class Account implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @NotNull
+    private Double transactionLimit;
+
+    @NotNull
+    private Double balance;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @NotNull
+    private LocalDateTime creationDate;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @NotNull
+    private LocalDateTime updateDate;
+
+    @JsonIgnore
+    private boolean softDelete;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
