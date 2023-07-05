b54b062760bc3ed2da8267e0847b85650bc6eb71
Damian Pereyra <pereyra.damian89@gmail.com>
2023-07-05 11:01:40 -0300

    BBVAFSW3T3-23 | Quito Anot. Seguridad.

--- wallet/src/main/java/com/bbva/wallet/config/SecurityConfiguration.java
+++ wallet/src/main/java/com/bbva/wallet/config/SecurityConfiguration.java
@@ -2,18 +2,12 @@ package com.bbva.wallet.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-
-
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
