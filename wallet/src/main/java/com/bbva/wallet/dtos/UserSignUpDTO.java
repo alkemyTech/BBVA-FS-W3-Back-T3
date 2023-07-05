4cb93fa524a196f692e910939c80a8655e227fe1
Damian Pereyra <pereyra.damian89@gmail.com>
2023-07-05 17:21:07 -0300

    BBVAFSW3T3-23 | Creo endpoint/service/dto de registro de usuarios + logica CBU

---
+++ wallet/src/main/java/com/bbva/wallet/dtos/UserSignUpDTO.java
@@ -0,0 +1,27 @@
+package com.bbva.wallet.dtos;
+
+
+import com.bbva.wallet.enums.RoleName;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSignUpDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+
+}
