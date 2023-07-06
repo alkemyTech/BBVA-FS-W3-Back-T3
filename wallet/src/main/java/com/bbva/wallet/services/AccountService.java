988ec8287fb5309f79535cc46c108eb1b5a2d949
robyspr <robysprenger@gmail.com>
2023-07-06 11:52:11 -0300

    BBVAFSW3T3-32 | Agregar metodo para buscar accounts de un user en AccountService

---
+++ wallet/src/main/java/com/bbva/wallet/services/AccountService.java
@@ -0,0 +1,16 @@
+package com.bbva.wallet.services;
+
+import com.bbva.wallet.entities.Account;
+import com.bbva.wallet.repositories.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public List<Account> getUserAccounts(Long userId) {
+        return accountRepository.findByUserId(userId);
+    }
+}
