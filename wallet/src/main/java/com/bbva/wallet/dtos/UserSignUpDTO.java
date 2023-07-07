0ff1a80d5e60717a58f42e60a2017f39cd144806
Damian Pereyra <pereyra.damian89@gmail.com>
2023-07-07 14:56:38 -0300

    BBVAFSW3T3-29 | Agrego valid. DTO

--- wallet/src/main/java/com/bbva/wallet/dtos/UserSignUpDTO.java
+++ wallet/src/main/java/com/bbva/wallet/dtos/UserSignUpDTO.java
@@ -4,6 +4,8 @@ package com.bbva.wallet.dtos;
 import com.bbva.wallet.enums.RoleName;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +17,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserSignUpDTO {
 
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
+
+    @NotNull
+    @Email
     private String email;
+
+    @NotNull
     private String password;
 
     @Enumerated(EnumType.STRING)
