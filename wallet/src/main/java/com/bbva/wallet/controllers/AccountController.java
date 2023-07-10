e6484ab93779f6261e5090977f10d366a1e6d721
robyspr <robysprenger@gmail.com>
2023-07-10 12:56:37 -0300

    BBVAFSW3T3-26 | Agrego constantes de limites y extraer usuario del token

--- wallet/src/main/java/com/bbva/wallet/controllers/AccountController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/AccountController.java
@@ -2,11 +2,15 @@ package com.bbva.wallet.controllers;
 
 import com.bbva.wallet.dtos.AccountDTO;
 import com.bbva.wallet.entities.Account;
+import com.bbva.wallet.entities.User;
 import com.bbva.wallet.enums.Currency;
 import com.bbva.wallet.services.AccountService;
+import com.bbva.wallet.utils.Utils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +18,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
-    private final UserService userService;
+    private final Utils utils;
+    @Value("${transaction.limit.ars}")
+    private Double transactionLimitArs;
+    @Value("${transaction.limit.usd}")
+    private Double transactionLimitUsd;
+    @Value("${initial.balance}")
+    private Double initialBalance;
 
     @PostMapping
-    public ResponseEntity<Account> createAccount(@Valid @RequestBody AccountDTO accountDTO) {
-        Long user_id = 0L;
+    public ResponseEntity<Account> createAccount(@Valid @RequestBody AccountDTO accountDTO, Authentication authentication) {
+        User userLoggedIn = (User) authentication.getPrincipal();
+        Long user_id = userLoggedIn.getId();
         Currency currency = accountDTO.getCurrency();
+
         var userAccounts = accountService.getUserAccounts(user_id);
 
         if (userAccounts.stream().anyMatch(
@@ -29,9 +41,9 @@ public class AccountController {
 
         Account newAccount =  Account.builder()
                 .currency(currency)
-                .transactionLimit( currency.equals(Currency.ARS) ? 300000.0 : 1000.0 )
-                .balance(0.0)
-                .user(userService.getUserById(user_id))
+                .transactionLimit( currency.equals(Currency.ARS) ? transactionLimitArs : transactionLimitUsd )
+                .balance(initialBalance)
+                .user(userLoggedIn)
                 .cbu(utils.generateRandomCbu())
                 .build();
         return ResponseEntity.ok(newAccount);
