1a385d0c288c2794352511fb8720a7915b023d28
robyspr <robysprenger@gmail.com>
2023-07-10 17:33:05 -0300

    BBVAFSW3T3-26 | Fix: uso de annotation @Preauthorize - funciona

--- wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
@@ -1,11 +1,8 @@
 package com.bbva.wallet.controllers;
 
-import com.bbva.wallet.entities.User;
-import com.bbva.wallet.enums.RoleName;
 import com.bbva.wallet.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +14,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 public class UserController {
     private final UserService userService;
 
-//    @PreAuthorize("hasRole('ADMIN')") todo: revisar, no filtra por rol ni por id
+    @PreAuthorize("hasAuthority('ADMIN') or #id == authentication.principal.getId ")
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable Long id, Authentication authentication ) {
-        User userLoggedIn = (User) authentication.getPrincipal();
-        if( userLoggedIn.getId() != id && !(userLoggedIn.getAuthorities().contains(RoleName.ADMIN) )) {
-            return ResponseEntity.status(403).body("No tienes permisos para eliminar este usuario");
-        }
+    public ResponseEntity<String> deleteById(@PathVariable Long id ) {
         userService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
