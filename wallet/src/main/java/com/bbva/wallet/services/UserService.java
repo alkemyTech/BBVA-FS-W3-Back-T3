4e25222d08b73266147168cee6fc7e3053aa4a19
robyspr <robysprenger@gmail.com>
2023-07-10 13:43:28 -0300

    BBVAFSW3T3-26 | Refactor de UserService

--- wallet/src/main/java/com/bbva/wallet/services/UserService.java
+++ wallet/src/main/java/com/bbva/wallet/services/UserService.java
@@ -1,18 +1,5 @@
 package com.bbva.wallet.services;
 
-import com.bbva.wallet.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
-    public void deleteById(Long id) {
-        var user = userRepository.findById(id);
-        user.ifPresent(value -> {
-            value.setSoftDelete(true);
-            userRepository.save(value);
-        });
-    }
+public interface UserService {
+    void deleteById(Long id);
 }
