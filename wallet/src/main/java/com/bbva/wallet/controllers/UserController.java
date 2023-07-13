593899f3b505e3db8008c3744efa1c63d748a1bd
DiegoApro <diegoaprosoff@gmail.com>
2023-07-13 15:38:48 -0300

    BBVAFSW3T3-47 | Paginar listado de usuarios Controller

--- wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
@@ -4,6 +4,13 @@ import com.bbva.wallet.entities.User;
 import com.bbva.wallet.services.UserService;
 import com.bbva.wallet.services.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,22 +23,24 @@ public class UserController {
     private final UserService userService;
     private final AccountService accountService;
 
-
-
-   @PreAuthorize("hasAuthority('ADMIN')")
-   @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        List<User> users = userService.getAll();
-        return ResponseEntity.ok(users);
-    }
-
-
-    
     @PreAuthorize("hasAuthority('ADMIN') or #id == authentication.principal.getId ")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id ) {
         userService.softDeleteById(id);
         accountService.softDeleteByUserId(id);
         return ResponseEntity.noContent().build();
+    }
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping
+    public ResponseEntity<PagedModel<EntityModel<User>>> getAllUser(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            PagedResourcesAssembler<User> pagedAssembler) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+        Page<User> userPage = userService.getAllUser(pageable);
+
+        PagedModel<EntityModel<User>> pageModel = pagedAssembler.toModel(userPage);
+        return ResponseEntity.ok(pageModel);
     }
 }
