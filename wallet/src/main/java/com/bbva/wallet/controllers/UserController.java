dd4adf2c7750923b3e3de26104a9f1d884bd6678
robyspr <robysprenger@gmail.com>
2023-07-17 11:31:13 -0300

    BBVAFSW3T3-fixes | Refactor de GetUser endpoint para usar annotation Preauthorize

--- wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
@@ -5,7 +5,6 @@ import com.bbva.wallet.entities.User;
 import com.bbva.wallet.exeptions.ErrorCodes;
 import com.bbva.wallet.exeptions.TransactionException;
 import com.bbva.wallet.exeptions.UserException;
-import com.bbva.wallet.entities.User;
 import com.bbva.wallet.services.UserService;
 import com.bbva.wallet.services.AccountService;
 import lombok.RequiredArgsConstructor;
@@ -48,13 +47,11 @@ public class UserController {
     }
 
     @SneakyThrows
+    @PreAuthorize("hasAuthority('ADMIN') or #id == authentication.principal.getId")
     @GetMapping("/{id}")
-    public ResponseEntity<User> allUsers(@PathVariable("id") Long id, Authentication authentication) {
-        User userLoggedIn = (User) authentication.getPrincipal();
-        User user= userService.findById(id).orElseThrow(() -> new TransactionException("No existe el usuario indicado ", ErrorCodes.USER_DOESNT_EXIST));
-        if (!Objects.equals(user.getId(), userLoggedIn.getId())){
-            throw new UserException("No se puede ver un usuario ajeno ", ErrorCodes.USER_DOESNT_EXIST);
-        }
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+        User user = userService.findById(id).orElseThrow(() -> new TransactionException("No existe el usuario " +
+                "indicado ", ErrorCodes.USER_DOESNT_EXIST));
 
         return ResponseEntity.ok(user);
     }
