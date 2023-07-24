b210ed2d3d067c30c4ed1770762352a3f33849e7
robyspr <robysprenger@gmail.com>
2023-07-24 15:31:05 -0300

    Fix del Cors

--- wallet/src/main/java/com/bbva/wallet/controllers/FixedTermDepositsController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/FixedTermDepositsController.java
@@ -16,10 +16,7 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/fixedTerm")
